64db08415c0f476c17a803954c3fba1e75bf33ca
m7medalgandy <142119759+m7medalgandy@users.noreply.github.com>
2024-06-29 18:42:02 +0300

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -2,33 +2,19 @@ import type { Config } from './types';
 
 export default {
   version: '1.0',
-  name: 'My Blog',
-  shortName: 'My Blog',
-  description: 'My blog description goes here...',
+  name: 'كل حصري',
+  shortName: 'كل حصري',
+  description: 'تطبيق موقع كل حصري لتصفح المحتوي بكل سهولة وسرعة وبدون اعلانات.',
   direction: 'auto',
-  language: 'en-US',
+  language: 'ar_AR',
   backgroundColor: '#fff',
   themeColor: '#fff',
   display: 'standalone',
   orientation: 'any',
   scope: '/',
-  startUrl: '/?utm_source=homescreen',
+  startUrl: '/?view=app',
   appleStatusBarStyle: 'black-translucent',
   preferRelatedApplications: false,
-  shortcuts: [
-    {
-      name: 'Shortcut 1',
-      shortName: 'Shortcut 1',
-      description: 'Shortcut 1 description goes here...',
-      url: '/search/label/shortcut-1?utm_source=homescreen',
-    },
-    {
-      name: 'Shortcut 2',
-      shortName: 'Shortcut 2',
-      description: 'Shortcut 2 description goes here...',
-      url: '/search/label/shortcut-2?utm_source=homescreen',
-    },
-  ],
   pwa: {
     logs: true,
     // OneSignal is not available if you are not using cloudflare workers
